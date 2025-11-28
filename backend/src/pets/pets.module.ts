403c07ecdfb17e6a039605619227bfcad8d4617c
Andre-Kilesse <kilesseandre@gmail.com>
2025-11-28 12:37:52 -0300

    Remocao das Seeds

--- backend/src/pets/pets.module.ts
+++ backend/src/pets/pets.module.ts
@@ -3,11 +3,10 @@ import { PetsService } from './pets.service';
 import { PetsController } from './pets.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Pet } from './entities/pet.entity';
-import { PetsSeed } from './pets.seed';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Pet])],
   controllers: [PetsController],
-  providers: [PetsService, PetsSeed],
+  providers: [PetsService],
 })
 export class PetsModule { }
